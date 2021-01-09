3142aa777cf374f2135fb48c69d614b701d67cff
Adrian Piwin <adrian7048@gmail.com>
2021-01-09 12:18:06 -0500

    Added basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement Settings")]
+    [SerializeField] private float speed;
+    [SerializeField] private float teleportRadius;
+    [SerializeField] private float teleportTime;
+    [SerializeField] private float fallMultiplier;
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        movement();
+    }
+
+    private void movement()
+    {
+        body.velocity = new Vector2(speed, body.velocity.y);
+
+        // Better falling
+        if (body.velocity.y < 0)
+        {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
